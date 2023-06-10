6f5ae1b98289576ae2122e55e934e3c2e9e7a11f
alpha <alphamodeyt@gmail.com>
2023-06-09 22:28:03 -0500

    1.20 update

--- src/main/java/ca/spottedleaf/starlight/common/blockstate/ExtendedAbstractBlockState.java
+++
@@ -1,9 +0,0 @@
-package ca.spottedleaf.starlight.common.blockstate;
-
-public interface ExtendedAbstractBlockState {
-
-    public boolean isConditionallyFullOpaque();
-
-    public int getOpacityIfCached();
-
-}
